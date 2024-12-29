9e702ba8c1c746a065379c8c021a55ef071cc979
divya9658 <23mh1a05m4@acoe.edu.in>
2024-12-29 21:17:36 +0530

    Add files via upload

---
+++ BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+a = input("What's the name of the city you grow up in? ")
+b = input("What's your pet name? ")
+print(f"Your brand name could be {a} {b}")
